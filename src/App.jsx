c5e85fba7a8d1ff8d4823f0ced3b7a5ae8060712
gustavo <gustavo@gmail.com>
2025-08-08 08:08:06 -0300

    acabei

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 export function App(){
+const [numero1, setNumero1] = useState(0)
+const [numero2, setNumero2] = useState(0)
+const[resultado, setresultado] = useState(0)
+
+function somar(e){
+  e.preventDefault()
+  setresultado(numero1 + numero2)
+}
+
   <main>
     <form action="">
-      <input type="number" placeholder="Numero 1" />
+      <input type="number" placeholder="Numero 1" onChange={(e) => setNumero1(e.target.valueAsNumber) } />
+      <input type="number" placeholder="Numero 2" onChange={(e) => setNumero2(e.target.valueAsNumber)  } />
+
+      <button onClick={somar}>somar</button>
+
     </form>
+
+    <p>resultado: {resultado}</p>
   </main>
 }
